4928146b4eb0a69f68f60261f5dd656c85b8aa72
Lucas <lucas@POSPEL241.local>
2021-09-28 10:36:49 -0300

    feature(courses): typeorm entity for courses and pg integration added

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
-
-@Module({ controllers: [CoursesController], providers: [CoursesService] })
+import { Course } from './course.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [TypeOrmModule.forFeature([Course])],
+  controllers: [CoursesController],
+  providers: [CoursesService],
+})
 export class CoursesModule {}
